de286e5c4474e9dc127eeb7c379883c7b22ddbfc
wibus-wee <1596355173@qq.com>
2024-12-14 21:40:25 +0800

    feat: enhance authentication decorators with Swagger integration
    - Added ApiBearerAuth and ApiUnauthorizedResponse decorators to the Auth function.
    - Improved API documentation for unauthorized access handling.

--- Way Tech+复赛+OpenHealth-开源项目健康度分析平台/open-health-backend/src/common/decorators/auth.decorator.ts
+++ Way Tech+复赛+OpenHealth-开源项目健康度分析平台/open-health-backend/src/common/decorators/auth.decorator.ts
@@ -1,11 +1,16 @@
 import { applyDecorators, UseGuards } from '@nestjs/common'
 
+import { ApiBearerAuth, ApiUnauthorizedResponse } from '@nestjs/swagger'
 import { JWTAuthGuard } from '../guard/auth.guard'
 
 export function Auth() {
   const decorators: (ClassDecorator | MethodDecorator | PropertyDecorator)[] =
     []
 
-  decorators.push(UseGuards(JWTAuthGuard))
+  decorators.push(
+    UseGuards(JWTAuthGuard),
+    ApiBearerAuth(),
+    ApiUnauthorizedResponse({ description: 'Unauthorized' }),
+  )
   return applyDecorators(...decorators)
 }
